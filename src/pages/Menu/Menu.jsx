7751abcb55ba6ca5d780c18b3e7f537acae58c34
Stepan Dordiai <stepandordiai@gmail.com>
2025-07-24 15:43:11 +0200

    Update Menu

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,20 +1,7 @@
 import { Helmet } from "react-helmet-async";
-import img1 from "/starters-01.jpg";
-import img2 from "/starters-02.jpg";
-import img3 from "/starters-03.jpg";
-import img4 from "/breakfast-01.jpg";
-import img5 from "/breakfast-02.jpg";
-import img6 from "/breakfast-03.jpg";
-import img7 from "/breakfast-04.jpg";
-import img8 from "/dinner-01.jpg";
-import img9 from "/dinner-02.jpg";
-import img10 from "/dinner-03.jpg";
-import img11 from "/dinner-05.jpg";
-import img12 from "/drinks-01.jpg";
-import img13 from "/drinks-02.jpg";
-import img14 from "/drinks-03.jpg";
-import "./Menu.scss";
 import { useEffect } from "react";
+import menuData from "./../../assets/data/menu-data.json";
+import "./Menu.scss";
 
 const Menu = () => {
 	useEffect(() => {
@@ -87,293 +74,119 @@ const Menu = () => {
 					<p className="menu__inner-title" id="starters">
 						Starters
 					</p>
-					<div
-						className="menu__item menu__item-special"
-						data-special-value="Starter of the Day"
-					>
-						<div className="menu__item-img-wrapper">
-							<img src={img1} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Tomato toast</p>
-								<p>$29.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img2} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Noodle Soup</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img3} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Pumpkin Soup</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
+					{menuData
+						.filter((item) => item.type === "starters")
+						.map((item) => {
+							return (
+								<div
+									key={item.id}
+									className={`menu__item ${
+										item.special && "menu__item-special"
+									}`}
+									data-special-value={item.special}
+								>
+									<div className="menu__item-img-wrapper">
+										<img src={item.img} alt="" />
+									</div>
+									<div className="menu__item-info">
+										<div className="menu__item-info-top">
+											<p>{item.name}</p>
+											<p>$ {(item.priceCents / 100).toFixed(2)}</p>
+										</div>
+										<p className="menu__item-info-bottom">
+											Lorem ipsum dolor sit amet, consectetur
+										</p>
+									</div>
+								</div>
+							);
+						})}
 				</div>
 				<div className="menu__inner">
 					<p className="menu__inner-title" id="lunch">
 						Lunch
 					</p>
-					<div
-						className="menu__item menu__item-special"
-						data-special-value="Lunch of the Day"
-					>
-						<div className="menu__item-img-wrapper">
-							<img src={img4} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Delicious Pancakes</p>
-								<p>$6.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img5} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Sweet Heaven</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img6} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Oatmeal Spirit</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img7} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Avocado Smash</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
+					{menuData
+						.filter((item) => item.type === "breakfast")
+						.map((item) => {
+							return (
+								<div
+									key={item.id}
+									className="menu__item"
+									data-special-value="Starter of the Day"
+								>
+									<div className="menu__item-img-wrapper">
+										<img src={item.img} alt="" />
+									</div>
+									<div className="menu__item-info">
+										<div className="menu__item-info-top">
+											<p>{item.name}</p>
+											<p>$ {(item.priceCents / 100).toFixed(2)}</p>
+										</div>
+										<p className="menu__item-info-bottom">
+											Lorem ipsum dolor sit amet, consectetur
+										</p>
+									</div>
+								</div>
+							);
+						})}
 				</div>
 				<div className="menu__inner">
 					<p className="menu__inner-title" id="dinner">
 						Dinner
 					</p>
-					<div
-						className="menu__item menu__item-special"
-						data-special-value="Dinner of the Day"
-					>
-						<div className="menu__item-img-wrapper">
-							<img src={img8} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Italian Pizza</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img9} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Sea Curry</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img10} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Vegan Burger</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img11} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Noodle Bowl</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-				</div>
-				<div className="menu__inner">
-					<p className="menu__inner-title" id="wine">
-						Wine
-					</p>
-					<div
-						className="menu__item menu__item-special"
-						data-special-value="Wine of the Day"
-					>
-						<div className="menu__item-img-wrapper">
-							<img src={img1} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Tomato toast</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img1} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Tomato toast</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img1} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Tomato toast</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img1} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Tomato toast</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
+					{menuData
+						.filter((item) => item.type === "dinner")
+						.map((item) => {
+							return (
+								<div
+									key={item.id}
+									className="menu__item"
+									data-special-value="Starter of the Day"
+								>
+									<div className="menu__item-img-wrapper">
+										<img src={item.img} alt="" />
+									</div>
+									<div className="menu__item-info">
+										<div className="menu__item-info-top">
+											<p>{item.name}</p>
+											<p>$ {(item.priceCents / 100).toFixed(2)}</p>
+										</div>
+										<p className="menu__item-info-bottom">
+											Lorem ipsum dolor sit amet, consectetur
+										</p>
+									</div>
+								</div>
+							);
+						})}
 				</div>
 				<div className="menu__inner">
 					<p className="menu__inner-title" id="drinks">
 						Drinks
 					</p>
-					<div
-						className="menu__item menu__item-special"
-						data-special-value="Drink of the Day"
-					>
-						<div className="menu__item-img-wrapper">
-							<img src={img12} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Panthouse Tonic</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img13} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Apple Breeze</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
-					<div className="menu__item">
-						<div className="menu__item-img-wrapper">
-							<img src={img14} alt="" />
-						</div>
-						<div className="menu__item-info">
-							<div className="menu__item-info-top">
-								<p>Frenchman Blitz</p>
-								<p>$5.00</p>
-							</div>
-							<p className="menu__item-info-bottom">
-								Lorem ipsum dolor sit amet, consectetur
-							</p>
-						</div>
-					</div>
+					{menuData
+						.filter((item) => item.type === "drinks")
+						.map((item) => {
+							return (
+								<div
+									key={item.id}
+									className="menu__item"
+									data-special-value="Starter of the Day"
+								>
+									<div className="menu__item-img-wrapper">
+										<img src={item.img} alt="" />
+									</div>
+									<div className="menu__item-info">
+										<div className="menu__item-info-top">
+											<p>{item.name}</p>
+											<p>$ {(item.priceCents / 100).toFixed(2)}</p>
+										</div>
+										<p className="menu__item-info-bottom">
+											Lorem ipsum dolor sit amet, consectetur
+										</p>
+									</div>
+								</div>
+							);
+						})}
 				</div>
 			</div>
 		</>
